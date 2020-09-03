2acc1ffe2607a5d187bea6daf93f111090e751ce
pankaj0590 <pankaj0590@gmail.com>
2020-09-04 00:36:20 +0530

    Update deployToECS.sh

--- scripts/deployToECS.sh
+++ scripts/deployToECS.sh
@@ -6,7 +6,7 @@
 dockerRepo=`aws ecr describe-repositories --repository-name dev-spring-boot-api-talk-to-db --region us-east-1 | grep repositoryUri | cut -d "\"" -f 4`
 dockerTag=`aws ecr list-images --repository-name dev-spring-boot-api-talk-to-db --region us-east-1 | grep imageTag | head -n 1 | cut -d "\"" -f 4`
 sed -e "s;DOCKER_IMAGE_NAME;${repositoryURI};g" scripts/template.json > taskDefinition.json
-aws ecs create-cluster --cluster-name EC2-cluster
+#aws ecs create-cluster --cluster-name EC2-cluster
 aws ecs register-task-definition --family spring-boot-api-talk-to-db --cli-input-json file://taskDefinition.json --region us-east-1
 aws ecs create-service --cluster EC2-cluster --service-name spring-boot-hello-world-service --task-definition spring-boot-hello-world --desired-count 1
 revision=`aws ecs describe-task-definition --task-definition spring-boot-api-talk-to-db --region us-east-1 | grep "revision" | tr -s " " | cut -d " " -f 3`
