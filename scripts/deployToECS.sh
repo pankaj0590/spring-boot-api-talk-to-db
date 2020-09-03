e723776325a549b6adb7d764318584eb2de22f18
pankaj0590 <pankaj0590@gmail.com>
2020-09-04 00:43:20 +0530

    Update deployToECS.sh

--- scripts/deployToECS.sh
+++ scripts/deployToECS.sh
@@ -8,6 +8,6 @@ dockerTag=`aws ecr list-images --repository-name dev-spring-boot-api-talk-to-db 
 sed -e "s;DOCKER_IMAGE_NAME;${repositoryURI};g" scripts/template.json > taskDefinition.json
 #aws ecs create-cluster --cluster-name EC2-cluster
 aws ecs register-task-definition --family spring-boot-api-talk-to-db --cli-input-json file://taskDefinition.json --region us-east-1
-aws ecs create-service --cluster EC2-cluster --service-name spring-boot-hello-world-service --task-definition spring-boot-hello-world --desired-count 1
+aws ecs create-service --cluster EC2-cluster --service-name spring-boot-api-talk-to-db-service --task-definition spring-boot-api-talk-to-db --desired-count 1
 revision=`aws ecs describe-task-definition --task-definition spring-boot-api-talk-to-db --region us-east-1 | grep "revision" | tr -s " " | cut -d " " -f 3`
 aws ecs update-service --cluster EC2-cluster --service spring-boot-api-talk-to-db-service --task-definition spring-boot-api-talk-to-db:${revision} --desired-count 1
